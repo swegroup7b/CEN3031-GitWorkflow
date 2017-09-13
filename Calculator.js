c4f946c2ea31a1537071d251a7a37e58ba116b5f
kapooramanpreet <kapoorise@gmail.com>
2017-09-13 15:08:37 -0400

    Template for Discussion Session

---
+++ Calculator.js
@@ -0,0 +1,25 @@
+function add(){
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1+v2).toString());
+}
+
+function subtract(){
+	//Implement this
+	
+	
+}
+
+function multiply(){
+	//Implement this
+	
+	
+}
+
+function divide(){
+	//Implement this
+	
+	
+	
+}
+		
