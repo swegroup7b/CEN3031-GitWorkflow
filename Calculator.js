8e9332c13939c2e5b23f372f54f5d3fab3c55703
Yilu Bao <yilu0331@ufl.edu>
2017-09-19 14:35:42 -0400

    add multiply feature

--- Calculator.js
+++ Calculator.js
@@ -12,8 +12,9 @@ function subtract(){
 
 function multiply(){
 	//Implement this
-	
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1*v2).toString());
 }
 
 function divide(){
