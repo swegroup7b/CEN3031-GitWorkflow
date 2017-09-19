a149407547a9b2b4561d1519f3a8b8519931f1c6
Caleb Robey <calebrobey118@gmail.com>
2017-09-19 14:34:15 -0400

    Commit of the subtraction sign (first one)

--- Calculator.js
+++ Calculator.js
@@ -6,20 +6,21 @@ function add(){
 
 function subtract(){
 	//Implement this
-	
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1-v2).toString());
+
 }
 
 function multiply(){
 	//Implement this
-	
-	
+
+
 }
 
 function divide(){
 	//Implement this
-	
-	
-	
+
+
+
 }
-		
